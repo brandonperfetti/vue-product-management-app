4c5212a17879ad56808b754f84a9a2401203dd7d
Brandon Perfetti <brandon@brandonperfetti.com>
2024-07-20 12:00:51 -0700

    Getting started with Shadcn UI and Vue.js.

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
 
 import vue from '@vitejs/plugin-vue'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [VueRouter(), vue()],
   resolve: {
     alias: {
