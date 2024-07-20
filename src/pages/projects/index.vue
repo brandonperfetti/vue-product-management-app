bd1fbf1f0d1ed1e132955fc8df77cf8b82ae5bf2
Brandon Perfetti <brandon@brandonperfetti.com>
2024-07-20 11:48:38 -0700

    Introduce Vue.js Reactivity system using Refs. Provide TypeScriptType Definitions for Refs in Vue.js Script Setup. Add Typescript support to Supabase in Vue.js. Create a new page for Tasks with database migration, seed and types.

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,8 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+import type { Tables } from '../../../database/types'
+
+const projects = ref<Tables<'projects'>[] | null>(null)
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+  if (error) console.error(error)
+  projects.value = data
+})()
+</script>
 
 <template>
   <div>
     <h1>Projects Page</h1>
     <RouterLink to="/">Go to home</RouterLink>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.name }}
+      </li>
+    </ul>
   </div>
 </template>
