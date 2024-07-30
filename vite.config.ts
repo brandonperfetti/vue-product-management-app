9a640cfe894fefdcc8d5de8d9c4a0c7fe316590d
Brandon Perfetti <brandon@brandonperfetti.com>
2024-07-30 10:26:16 -0700

    Integrate unplugin-auto-import with Vue and Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,24 +2,39 @@ import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
 
 import vue from '@vitejs/plugin-vue'
-import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   css: {
     postcss: {
-      plugins: [tailwind(), autoprefixer()],
-    },
-  },
-  plugins: [VueRouter(), vue({
-    template: {
-      compilerOptions: {
-        isCustomElement: element => element.startsWith('iconify-icon')
-      }
+      plugins: [tailwind(), autoprefixer()]
     }
-  })],
+  },
+  plugins: [
+    VueRouter(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      imports: ['vue', 'vue-router'],
+      dts: true,
+      viteOptimizeDeps: true
+    }),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (element) => element.startsWith('iconify-icon')
+        }
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
